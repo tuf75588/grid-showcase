f6b5279559271d6ad2ebe301d667ce2e763fb86f
Andrew Davis <atd285@gmail.com>
2022-05-10 16:21:59 -0400

    clear display function

--- 07-calculator/index.js
+++ 07-calculator/index.js
@@ -18,6 +18,11 @@ function inputNumbers() {
   for (const button of buttons) {
     button.addEventListener('click', ({ target: { innerText } }) => {
       const val = innerText; // the number we click
+      console.log(val)
+      // check if we want to clear the screen
+      if (val === 'AC') {
+       return clearDisplay()
+      }
       screen.leftPart += val;
       // is the first press a .?
       display.textContent = screen.leftPart
@@ -26,3 +31,11 @@ function inputNumbers() {
 }
 
 inputNumbers();
+
+
+function clearDisplay() {
+  screen.leftPart = '';
+  screen.rightPart = '';
+  screen.operation = '';
+  display.textContent = 0
+}
